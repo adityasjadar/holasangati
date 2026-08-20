ada6a2603f140b5fc1a06c677264ce22955c5f6a
adityasjadar <adityasjadar@gmail.com>
2026-08-20 10:35:15 +0530

    Update Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -1,4 +1,4 @@
-import { useLanguage } from '../../i18n/LanguageContext';
+import { useLanguage } from '/LanguageContext';
 
 export default function Footer() {
   const { t, lang } = useLanguage();
